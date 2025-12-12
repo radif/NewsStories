393d352863c60a74e805ea4be1c7bd346d6d0e47
Radif Sharafullin <radif@replaydigital.net>
2025-12-11 22:01:28 -0800

    Initial Commit

---
+++ NewsStories/NewsStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsStoriesApp.swift
+//  NewsStories
+//
+//  Created by Radif Sharafullin on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
